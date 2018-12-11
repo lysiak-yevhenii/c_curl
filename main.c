2426d2021cb1a97bd0d17256299a324b30894588
jenuaz <lisyak.yevhenii@gmail.com>
2018-12-11 09:20:25 +0200

    Parse GET HTML via param as link

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
-int			main(void)
+int			main(int ac, char **av)
 {
 	CURL	 	*curl;
 	CURLcode	res;
@@ -9,12 +9,13 @@ int			main(void)
 	curl = curl_easy_init();
 	if (curl)
 	{
-		curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+		curl_easy_setopt(curl, CURLOPT_URL, av[1]);
 		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 		res = curl_easy_perform(curl);
 		if (res != CURLE_OK)
 			fprintf(stderr, "curl_easy_perform() failde: %s\n", curl_easy_strerror(res));
 		curl_easy_cleanup(curl);
+		printf("%s", av[1]);
 	}
 	return (0);
 }
